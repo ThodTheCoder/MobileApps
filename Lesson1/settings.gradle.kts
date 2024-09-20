ebe35b475c4ccd3996e52c8eadcb610338129d7c
ThodTheCoder <nelizarov345@gmail.com>
2024-09-20 15:44:39 +0300

    Lesson 1

---
+++ Lesson1/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobileApps"
+include(":app")
+include(":layout_type")
+include(":control_lesson1")
+include(":buttonclicker")
+include(":buttonclicker")
+include(":button_clicker")
